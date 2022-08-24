94480aa34c1cb3c8f97864a0ce831b68add1a572
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-08-24 20:28:10 -0300

    Criando componente Tasks, mas ainda não foi concluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Header } from './components/Header'
+import { PlusCircle } from 'phosphor-react'
 import './Global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.create}>
+        <form>
+          <input type="text" placeholder='Adicione uma tarefa' className={styles.task} />
+          <button className={styles.buttonCreate}>
+            Criar
+            <div className={styles.plus}>
+              <PlusCircle size={16} />
+            </div>
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,19 @@
+import styles from './Tasks.module.css'
+
+export function Tasks() {
+    return(
+        <div className={styles.tasks}>
+            <span>
+
+            </span>
+
+            <label htmlFor="">
+
+            </label>
+
+            <button>
+                
+            </button>
+        </div>
+    )
+}
